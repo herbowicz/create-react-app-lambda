03cb585b1532adbc121355a53f7d14c059c61dbf
Greg Herbowicz <herbowicz@gmail.com>
2019-08-31 20:13:39 +0200

    sync fix longer

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,9 +1,7 @@
-// this uses the callback syntax, however, we encourage you to try the async/await syntax shown in async-dadjoke.js
-export function handler(event, context) {
+export function handler(event, context, callback) {
   console.log('queryStringParameters', event.queryStringParameters)
-  const resp = {
+  callback(null, {
     statusCode: 200,
     body: JSON.stringify({ msg: 'Hello, all' }),
-  }
-  return response
+  })
 }
